5d6668edb669fadf9a49ce6d0793b95da9cf5d7a
VlaStoBIT <vladica.stojic@bit.admin.ch>
2025-03-18 16:43:26 +0100

    Code rearranged

--- src/main/java/ch/admin/bj/swiyu/didtoolbox/CreateTdwCommand.java
+++ src/main/java/ch/admin/bj/swiyu/didtoolbox/CreateTdwCommand.java
@@ -30,6 +30,78 @@ class CreateTdwCommand {
     @Parameter(names = {"--force-overwrite", "-f"},
             description = "Overwrite existing PEM key files, if any")
     boolean forceOverwrite;
+    @Parameter(names = {"--identifier-registry-url", "-u"},
+            description = "A HTTP(S) DID URL (to did.jsonl) to create TDW DID log for",
+            required = true,
+            converter = IdentifierRegistryUrlParameterConverter.class,
+            validateWith = IdentifierRegistryUrlParameterValidator.class)
+    URL identifierRegistryUrl;
+    @Parameter(names = {"--method-version", "-m"},
+            description = "Defines the did:tdw specification version to use when generating a DID log. Currently supported is only '" + DEFAULT_METHOD_VERSION + "'",
+            defaultValueDescription = DEFAULT_METHOD_VERSION)
+    //,required = true)
+    String methodVersion;
+    @Parameter(names = {"--signing-key-file", "-s"},
+            description = "The ed25519 private key file corresponding to the public key, required to sign and output the initial DID log entry. In PEM Format",
+            converter = PemFileParameterConverter.class,
+            validateWith = PemFileParameterValidator.class)
+    File signingKeyPemFile;
+    @Parameter(names = {"--verifying-key-file", "-v"},
+            description = "The ed25519 public key file for the DID Document’s verification method. In PEM format",
+            listConverter = PemFileParameterListConverter.class,
+            //converter = PemFileParameterConverter.class,
+            validateWith = PemFileParameterValidator.class,
+            variableArity = true)
+    List<File> verifyingKeyPemFiles;
+
+    /*
+    static class OutputDirParameterConverter implements IStringConverter<File> {
+        @Override
+        public File convert(String value) {
+            return new File(value);
+        }
+    }
+
+    public static class OutputDirParameterValidator implements IParameterValidator {
+        @Override
+        public void validate(String name, String value) throws ParameterException {
+            File dir = new File(value);
+            if (dir.exists() && !dir.isDirectory()) {
+                throw new ParameterException("Parameter " + name + " should be a directory, not a file (found " + value + ")");
+            }
+        }
+    }
+
+    @Parameter(names = {"--key-pair-output-dir", "-o"},
+            description = "The directory to store the generated key pair (both in PEM Format), in case no external keys are supplied. Otherwise, ignored",
+            converter = OutputDirParameterConverter.class,
+            validateWith = OutputDirParameterValidator.class)
+    File outputDir;
+     */
+    @Parameter(names = {"--jks-file", "-j"},
+            description = "Java KeyStore (PKCS12) file to read the (signing/verifying) keys from",
+            converter = JksFileParameterConverter.class,
+            validateWith = JksFileParameterValidator.class)
+    File jksFile;
+    @Parameter(names = {"--jks-password"},
+            description = "Java KeyStore password used to check the integrity of the keystore, the password used to unlock the keystore",
+            password = true)
+    String jksPassword;
+    @Parameter(names = {"--jks-alias"},
+            description = "Java KeyStore alias")
+    String jksAlias;
+    @Parameter(names = {"--assert", "-a"},
+            description = "An assertion method (comma-separated) parameters: a key name as well as a PEM file containing EC P-256 public/verifying key",
+            listConverter = VerificationMethodParametersConverter.class,
+            validateWith = VerificationMethodKeyParametersValidator.class,
+            variableArity = true)
+    List<VerificationMethodParameters> assertionMethodKeys;
+    @Parameter(names = {"--auth", "-t"},
+            description = "An authentication method (comma-separated) parameters: a key name as well as a PEM file containing EC P-256 public/verifying key",
+            listConverter = VerificationMethodParametersConverter.class,
+            validateWith = VerificationMethodKeyParametersValidator.class,
+            variableArity = true)
+    List<VerificationMethodParameters> authenticationKeys;
 
     public static class IdentifierRegistryUrlParameterValidator implements IParameterValidator {
         @Override
@@ -59,58 +131,6 @@ class CreateTdwCommand {
         }
     }
 
-    @Parameter(names = {"--identifier-registry-url", "-u"},
-            description = "A HTTP(S) DID URL (to did.jsonl) to create TDW DID log for",
-            required = true,
-            converter = IdentifierRegistryUrlParameterConverter.class,
-            validateWith = IdentifierRegistryUrlParameterValidator.class)
-    URL identifierRegistryUrl;
-
-    @Parameter(names = {"--method-version", "-m"},
-            description = "Defines the did:tdw specification version to use when generating a DID log. Currently supported is only '" + DEFAULT_METHOD_VERSION + "'",
-            defaultValueDescription = DEFAULT_METHOD_VERSION)
-    //,required = true)
-    String methodVersion;
-
-    /*
-    static class OutputDirParameterConverter implements IStringConverter<File> {
-        @Override
-        public File convert(String value) {
-            return new File(value);
-        }
-    }
-
-    public static class OutputDirParameterValidator implements IParameterValidator {
-        @Override
-        public void validate(String name, String value) throws ParameterException {
-            File dir = new File(value);
-            if (dir.exists() && !dir.isDirectory()) {
-                throw new ParameterException("Parameter " + name + " should be a directory, not a file (found " + value + ")");
-            }
-        }
-    }
-
-    @Parameter(names = {"--key-pair-output-dir", "-o"},
-            description = "The directory to store the generated key pair (both in PEM Format), in case no external keys are supplied. Otherwise, ignored",
-            converter = OutputDirParameterConverter.class,
-            validateWith = OutputDirParameterValidator.class)
-    File outputDir;
-     */
-
-    @Parameter(names = {"--signing-key-file", "-s"},
-            description = "The ed25519 private key file corresponding to the public key, required to sign and output the initial DID log entry. In PEM Format",
-            converter = PemFileParameterConverter.class,
-            validateWith = PemFileParameterValidator.class)
-    File signingKeyPemFile;
-
-    @Parameter(names = {"--verifying-key-file", "-v"},
-            description = "The ed25519 public key file for the DID Document’s verification method. In PEM format",
-            listConverter = PemFileParameterListConverter.class,
-            //converter = PemFileParameterConverter.class,
-            validateWith = PemFileParameterValidator.class,
-            variableArity = true)
-    List<File> verifyingKeyPemFiles;
-
     static class PemFileParameterListConverter implements IStringConverter<List<File>> {
         @Override
         public List<File> convert(String value) {
@@ -119,35 +139,6 @@ class CreateTdwCommand {
             return fileList;
         }
     }
-
-    @Parameter(names = {"--jks-file", "-j"},
-            description = "Java KeyStore (PKCS12) file to read the (signing/verifying) keys from",
-            converter = JksFileParameterConverter.class,
-            validateWith = JksFileParameterValidator.class)
-    File jksFile;
-
-    @Parameter(names = {"--jks-password"},
-            description = "Java KeyStore password used to check the integrity of the keystore, the password used to unlock the keystore",
-            password = true)
-    String jksPassword;
-
-    @Parameter(names = {"--jks-alias"},
-            description = "Java KeyStore alias")
-    String jksAlias;
-
-    @Parameter(names = {"--assert", "-a"},
-            description = "An assertion method (comma-separated) parameters: a key name as well as a PEM file containing EC P-256 public/verifying key",
-            listConverter = VerificationMethodParametersConverter.class,
-            validateWith = VerificationMethodKeyParametersValidator.class,
-            variableArity = true)
-    List<VerificationMethodParameters> assertionMethodKeys;
-
-    @Parameter(names = {"--auth", "-t"},
-            description = "An authentication method (comma-separated) parameters: a key name as well as a PEM file containing EC P-256 public/verifying key",
-            listConverter = VerificationMethodParametersConverter.class,
-            validateWith = VerificationMethodKeyParametersValidator.class,
-            variableArity = true)
-    List<VerificationMethodParameters> authenticationKeys;
 
     static class PemFileParameterConverter implements IStringConverter<File> {
         @Override
